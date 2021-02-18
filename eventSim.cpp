2cce9f89259e3be7585f3a2748fb69e41c4a1f06
KasieMadden <44176460+KasieMadden@users.noreply.github.com>
2021-02-17 19:50:21 -0500

    removed the PQ I made because I went with the the one that was included with c++.

--- eventSim.cpp
+++ eventSim.cpp
@@ -33,90 +33,17 @@ struct node{
     struct node *tail;
 };
 
-
+//using included priority queue queue
 //using included FIFO queue
 //used cplusplus point as reference
 //https://www.cplusplus.com/reference/queue/queue/queue/
+//https://www.geeksforgeeks.org/priority-queue-in-cpp-stl/
+//http://www.cplusplus.com/reference/queue/priority_queue/
 std::priority_queue<node>jobQueue;
 std::queue<node>CPUqueue;
 std::queue<node>disk1Queue;
 std::queue<node>disk2Queue;
 std::queue<node>networkQueue;
-
-
-/**priorityQueue
-class priorityQueue{
-    //Private access method for head pointer
-    private:
-    node *head;
-
-    //Public access method
-    public:
-    priorityQueue(){
-        //initialized null
-        head = NULL;
-    }//end of priorityQueue()
-
-    //function to push into queue
-    //similar to the push function used in a stack
-    void push(string tID,int tType, int tTime){
-        node *temp;
-        node *temp2;
-
-        temp = new node;
-        temp -> jobID = tID;
-        temp -> type = tType;
-        temp -> time = tTime;
-
-        if(head == NULL || tTime < head -> time){
-            temp -> tail = head;
-            head = temp;
-        }//end of if
-        else{
-            temp2 = head;
-            while(temp2 -> tail != NULL && temp2 ->tail->time <= tTime ) {
-                temp2 = temp2->tail;
-            }//end of while
-            temp -> tail = temp2 -> tail;
-            temp2 -> tail = temp;
-
-        }//end of else
-    }//end of push
-
-        void empty(){
-            node *temp;
-            if(head == NULL){
-              cout<< "Queue Underflow\n";
-            }//end of if
-            else{
-                temp = head;
-                cout <<"Deleted item is: "<< temp -> type<<endl;
-                head = head -> tail;
-                free(temp);
-            }//end of else
-        }//end of empty()
-
-        void printQ(){
-        node *pointer;
-        pointer = head;
-
-            if(head == NULL){
-                cout<<"Queue is empty\n";
-
-            }//end of if
-            else{
-                cout<<"Queue is :\n";
-                cout<<"Priority Item\n";
-                while(pointer != NULL){
-                    cout<<pointer->time<<" "<<pointer->jobID<<endl;
-                    pointer = pointer ->tail;
-                }//end of while
-
-            }//end of else
-
-        }//end of printQ
-
-};// end of priorityQueue CLASS**/
 
 
 //random number generator
